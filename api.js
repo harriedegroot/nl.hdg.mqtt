19905f24b8814bb98d3bae0fa09309c2298828c5
Harrie de Groot <harriedegroot@gmail.com>
2021-03-26 22:59:39 +0100

    SDK3 fix: refactored app api

--- api.js
+++ api.js
@@ -2,179 +2,58 @@
 const Log = require('./Log');
 const Homey = require('homey');
 
-module.exports = [
-    {
-        description: 'Retrieve all devices with their information',
-        method: 'GET',
-        path: '/devices',
-        role: 'owner',
-        requires_authorization: true,
-        fn: function (args, callback) {
-            if (Homey.app) {
-                Homey.app.getDevices()
-                    .then(res => {
-                        callback(null, res);
-                    })
-                    .catch(error => {
-                        Log.error(error);
-                        callback(error, null);
-                    });
-            } else {
-                callback('App not ready, please try again');
-            }
-        }
+module.exports = {
+
+    // Retrieve all devices with their information
+    async getDevices({ homey }) {
+        return homey.app.getDevices();
     },
-    {
-        description: 'Retrieve all zones with their information',
-        method: 'GET',
-        path: '/zones',
-        role: 'owner',
-        requires_authorization: true,
-        fn: function (args, callback) {
-            if (Homey.app) {
-                Homey.app.getZones()
-                    .then(res => {
-                        callback(null, res);
-                    })
-                    .catch(error => {
-                        Log.error(error);
-                        callback(error, null);
-                    });
-            } else {
-                callback('App not ready, please try again');
-            }
-        }
+
+    // Retrieve all zones with their information
+    async getZones({ homey }) {
+        return homey.app.getZones();
     },
-    {
-        description: 'Fetch running state',
-        method: 'GET',
-        path: '/running',
-        fn: function (args, callback) {
-            if (Homey.app) {
-                let result = Homey.app.isRunning();
-                if (result instanceof Error) callback(result);
-                else callback(null, result);
-            } else {
-                callback(null, false);
-            }
-        }
+
+    // Fetch running state
+    async getRunning({ homey }) {
+        return homey.app.isRunning();
     },
-    {
-        description: 'Set running state',
-        method: 'POST',
-        path: '/running',
-        role: 'owner',
-        requires_authorization: true,
-        fn: function (args, callback) {
-            if (Homey.app) {
-                let running = args && args.body && args.body.running;
-                let result = Homey.app.setRunning(running);
-                if (result instanceof Error) callback(result);
-                else callback(null, result);
-            } else {
-                callback('App not ready, please try again');
-            }
-        }
+
+    // Set running state
+    async setRunning({ homey, body }) {
+        const running = body && body.running;
+        return homey.app.setRunning(running);
     },
-    {
-        description: 'Refresh device states',
-        method: 'GET',
-        path: '/refresh',
-        role: 'owner',
-        requires_authorization: true,
-        fn: function (args, callback) {
-            if (Homey.app) {
-                let result = Homey.app.refresh()
-                    .then(() => {
-                        if (result instanceof Error) callback(result);
-                        else callback(null, result);
-                    })
-                    .catch(error => callback(error));
-            } else {
-                callback('App not ready, please try again');
-            }
-        }
+
+    // Refresh device states
+    async refresh({ homey }) {
+        return await homey.app.refresh();
     },
-    {
-        description: 'Settings changed',
-        method: 'GET',
-        path: '/settings_changed',
-        role: 'owner',
-        requires_authorization: true,
-        fn: function (args, callback) {
-            if (Homey.app) {
-                let result = Homey.app.settingsChanged();
-                if (result instanceof Error) callback(result);
-                else callback(null, result);
-            } else {
-                callback('App not ready, please try again');
-            }
-        }
+
+    // Settings changed
+    async getSettingsChanged({ homey }) {
+        return await homey.app.settingsChanged();
     },
-    {
-        description: 'Log lines',
-        method: 'GET',
-        path: '/log',
-        role: 'owner',
-        requires_authorization: true,
-        fn: function (args, callback) {
-            if (Log) {
-                let result = Log.getLogLines();
-                if (result instanceof Error) callback(result);
-                else callback(null, result);
-            } else {
-                callback(null, '');
-            }
-        }
+
+    // Log lines
+    async getLog({ homey }) {
+        return Log.getLogLines();
     },
-    {
-        description: 'Log level',
-        method: 'GET',
-        path: '/loglevel',
-        role: 'owner',
-        requires_authorization: true,
-        fn: function (args, callback) {
-            if (Log) {
-                let result = Log.getLevel();
-                if (result instanceof Error) callback(result);
-                else callback(null, result);
-            } else {
-                callback(null, '');
-            }
-        }
+    
+    // Log level
+    async getLogLevel({ homey }) {
+        return Log.getLevel();
     },
-    {
-        description: 'Set log level',
-        method: 'POST',
-        path: '/loglevel',
-        role: 'owner',
-        requires_authorization: true,
-        fn: function (args, callback) {
-            if (Log && args && args.body) {
-                const level = args.body.level;
-                Log.info("Set log level: " + level);
-                let result = Log.setLevel(level);
-                if (result instanceof Error) callback(result);
-                else callback(null, result);
-            } else {
-                callback('App not ready, please try again');
-            }
-        }
+
+    // Set log level
+    async setLogLevel({ homey, body }) {
+        const level = body.level;
+        Log.info("Set log level: " + level);
+        return Log.setLevel(level);
     },
-    {
-        description: 'Queue/Progress state',
-        method: 'GET',
-        path: '/state',
-        role: 'owner',
-        requires_authorization: true,
-        fn: function (args, callback) {
-            if (Homey.app) {
-                let result = Homey.app.getState();
-                if (result instanceof Error) callback(result);
-                else callback(null, result);
-            } else {
-                callback('App not ready, please try again');
-            }
-        }
+
+    // Queue/Progress state
+    async getState({ homey }) {
+        return homey.app.getState();
     }
-];
+}
